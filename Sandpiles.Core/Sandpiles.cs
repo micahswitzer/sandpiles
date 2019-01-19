6d7a31f3eac6ed0fb9c3ac7be6cf7fa39c3a0c4b
Micah Switzer <micah.switzer.dev@outlook.com>
2019-01-18 19:31:40 -0500

    Initial commit

---
+++ Sandpiles.Core/Sandpiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sandpiles.Core
+{
+    public class Sandpiles
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public int[] DecayProbabilities { get; }
+        public int[] GrowthProbabilities { get; }
+        public int[][] Piles { get; }
+
+        public Sandpiles(int width, int height, int[] decayProbabilities, int[] growthProbabilities) {
+           Width = width;
+           Height = height;
+           DecayProbabilities = decayProbabilities;
+           GrowthProbabilities = growthProbabilities;
+        }
+
+        public void InitPiles() {
+            
+        }
+    }
+}
